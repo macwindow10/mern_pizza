7abe0affbf410e5a3dd9389456bd5cefe98ab650
mac.window.10 <mac.window.10@gmail.com>
2022-08-14 19:00:58 +0500

    working with static data

--- client/src/components/Pizza.js
+++ client/src/components/Pizza.js
@@ -1,8 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function Pizza({ pizza }) {
+
+    const [quantity, setQuantity] = useState(1);
+    const [varient, setVarient] = useState('small');
+
     return (
-        <div>
+        <div className='m-5'>
             <h1>
                 {pizza.name}
             </h1>
@@ -11,17 +15,17 @@ export default function Pizza({ pizza }) {
                 className="img-fluid"
                 style={{ height: "200px", width: "200px" }} />
             <div className="flex-container">
-                <div className='w-100'>
+                <div className='w-100 m-1'>
                     <p>Varients</p>
-                    <select>
+                    <select className='form-control' value={varient} onChange={(e) => { setVarient(e.target.value) }} >
                         {pizza.varients.map(varient => {
                             return <option value={varient} key={varient}>{varient}</option>
                         })}
                     </select>
                 </div>
-                <div className='w-100'>
+                <div className='w-100 m-1'>
                     <p>Quantity</p>
-                    <select>
+                    <select className='form-control' value={quantity} onChange={(e) => { setQuantity(e.target.value) }}>
                         {
                             [...Array(10).keys()].map((x, i) => {
                                 return <option value={i + 1} key={i + 1}>{i + 1}</option>
@@ -31,6 +35,16 @@ export default function Pizza({ pizza }) {
                     </select>
                 </div>
             </div>
+
+            <div className="flex-container">
+                <div className='m-1 w-100'>
+                    <h1>Price : {pizza.prices[0][varient] * quantity}</h1>
+                </div>
+                <div className='m-1 w-100'>
+                    <button className='btn'>Add to Cart</button>
+                </div>
+            </div>
+
         </div >
     )
 }
